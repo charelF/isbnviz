584e30080edbbe97d61b8e2133b43c0f539d3c6f
Charel Felten <charel.felten@gmail.com>
2025-01-28 21:40:34 +0100

    x

--- visualization/src/main.js
+++ visualization/src/main.js
@@ -9,13 +9,10 @@ import GeoJSON from 'ol/format/GeoJSON';
 import { getCenter } from 'ol/extent.js';
 import { FullScreen, defaults as defaultControls } from 'ol/control.js';
 import {
-    Circle as CircleStyle,
     Fill,
     Stroke,
     Style,
-    Text,
 } from 'ol/style.js';
-import Feature from 'ol/Feature';
 import { posToNum } from './util';
 
 const N = 2 ** 16
@@ -44,14 +41,8 @@ const vectorLayerAll = new VectorLayer({
 });
 vectorLayerAll.setOpacity(0.5)
 
-// Setup Zoomify source with adjusted positioning
-const isMobile = true///Mobi|Android/i.test(navigator.userAgent);
-const ratio = isMobile ? 1 : 2;
-const zdir = isMobile ? -1 : 1;
-// TODO: make the above settings
-
-console.log(isMobile, ratio, zdir)
-
+const ratio = 1
+const zdir = -1
 const source = new Zoomify({
     url: 'zoomify/combined.png/{TileGroup}/{z}-{x}-{y}.png',
     size: [N, N / 2],
@@ -83,11 +74,10 @@ const map = new Map({
     }),
 });
 
-function polygonStyleFunction(feature, resolution) {
-    const geometry = feature.getGeometry();
-    const centroid = geometry.getExtent();
-
-    return new Style({
+const vectorLayerHighlight = new VectorLayer({
+    source: new VectorSource(),
+    map: map,
+    style: new Style({
         stroke: new Stroke({
             color: '#000000',//feature.get('C7'),
             width: 2,
@@ -95,35 +85,23 @@ function polygonStyleFunction(feature, resolution) {
         fill: new Fill({
             color: '#00000010'//feature.get('C3'),
         }),
-    });
-}
-
-const vectorLayerHighlight = new VectorLayer({
-    source: new VectorSource(),
-    map: map,
-    style: polygonStyleFunction,
+    }),
 });
 
 let highlight;
 const displayFeatureInfo = function (event) {
-    console.log(event)
     const pixel = event.pixel
     const coordinates = event.coordinate;
     const feature = map.forEachFeatureAtPixel(pixel, function (feature) {
         return feature;
     });
-    // Set the tooltip position and content
-    const offset0 = 0; // Distance between the mouse and the tooltip
-    const offset1 = 0; // Distance between the mouse and the tooltip
-    info.style.left = pixel[0] - info.offsetWidth - offset0 + 'px'; // Offset to the left of the pointer
-    info.style.top = pixel[1] - offset1 + 'px'; // Vertically aligned with the pointer
-    info.style.visibility = 'visible';
     var x = Math.round(coordinates[0])
     var y = - Math.round(coordinates[1]) + 32768
     var i = posToNum(y, x)
     var isbn = getISBN(i)
-    // console.log(`x=${x} y=${y} i=${i}`)
-    info.innerText = `x=${x} y=${y} i=${i} isbn=${isbn} ${feature?.get('NAME') ?? ''}`;
+    const infoDisplay = document.getElementById('info-display');
+    infoDisplay.innerText = `x=${x.toString().padStart(5, '0')} y=${y.toString().padStart(5, '0')} i=${i} isbn=${isbn} \n ${feature?.get('NAME') ?? '-'}`;
+
 
     if (feature !== highlight) {
         if (highlight) {
@@ -138,7 +116,6 @@ const displayFeatureInfo = function (event) {
 
 map.on('pointermove', function (event) {
     if (event.dragging) {
-        info.style.visibility = 'hidden';  // for mobile
         return;
     }
     displayFeatureInfo(event);
@@ -155,15 +132,7 @@ map.on('click', function (event) {
     }
 });
 
-const info = document.createElement('div');
-info.id = 'info';
-document.body.appendChild(info);
-
 function getISBN(index) {
     const newIndex = index + 978_000_000_000;
     return newIndex.toString();  // Convert the result to a string
 }
-
-//   vectorLayerHighlight.setOpacity(0)
-//   vectorLayerAll.setOpacity(0)
-
